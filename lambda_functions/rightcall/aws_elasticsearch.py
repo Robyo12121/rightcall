a5ffdc71be07809d3a86c385d7eee07a43f0717b
Robin Staunton Collins <robinstauntoncollins@gmail.com>
2019-01-17 19:35:32 +0100

    Add new venvs to .gitignore

--- lambda_functions/rightcall/aws_elasticsearch.py
+++ lambda_functions/rightcall/aws_elasticsearch.py
@@ -1,11 +1,5 @@
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
-import configparser
-
-path = '~/.aws/credentials'
-config = configparser.ConfigParser()
-config.read(path)
-
 
 # REGION = 'eu-central-1'
 # host = 'https://search-rightcall-kzg2r25sxih6fyibq4alvpvilm.eu-central-1.es.amazonaws.com'
